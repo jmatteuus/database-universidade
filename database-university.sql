98272330d1508c3a857c418c0e77af188be1d526
João Mateus <joaomateuscvieira@gmail.com>
2024-12-09 12:33:50 -0300

    Tabela de alunos

--- database-university.sql
+++ database-university.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE universidade;
 
 USE universidade;
+
+CREATE TABLE alunos (
+cadastro INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(40) NOT NULL,
+nascimento DATE NOT NULL,
+sexo ENUM ('M','F'),
+nacionalidade VARCHAR(20) DEFAULT 'Brasil',
+endereco varchar(255) NOT NULL,
+matricula TINYINT(1) NOT NULL COMMENT '1 = Ativo, 0 = Inativo',
+email VARCHAR(50) NOT NULL,
+numero VARCHAR(18) NOT NULL,
+PRIMARY KEY (cadastro)
+)
