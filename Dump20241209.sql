ca2c471d112a308faaebede8e5abc155309c09f9
João Mateus <joaomateuscvieira@gmail.com>
2024-12-09 15:44:32 -0300

    dump

---
+++ Dump20241209.sql
@@ -0,0 +1,108 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alunos` (
+  `cadastro` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `nascimento` date NOT NULL,
+  `sexo` enum('M','F','OUTRO') DEFAULT 'OUTRO',
+  `nacionalidade` varchar(20) DEFAULT 'Brasil',
+  `endereco` varchar(255) NOT NULL,
+  `matricula` tinyint(1) NOT NULL COMMENT '1 = Ativo, 0 = Inativo',
+  `email` varchar(50) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  PRIMARY KEY (`cadastro`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alunos`
+--
+
+LOCK TABLES `alunos` WRITE;
+/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alunos_cursos`
+--
+
+DROP TABLE IF EXISTS `alunos_cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alunos_cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `data_inicio` date NOT NULL,
+  `data_fim` date NOT NULL,
+  `semestre` int NOT NULL,
+  `id_aluno` int DEFAULT NULL,
+  `id_curso` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_aluno` (`id_aluno`),
+  KEY `id_curso` (`id_curso`),
+  CONSTRAINT `alunos_cursos_ibfk_1` FOREIGN KEY (`id_aluno`) REFERENCES `alunos` (`cadastro`) ON DELETE CASCADE,
+  CONSTRAINT `alunos_cursos_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`idcurso`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alunos_cursos`
+--
+
+LOCK TABLES `alunos_cursos` WRITE;
+/*!40000 ALTER TABLE `alunos_cursos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alunos_cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `idcurso` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `tipo` enum('Bacharelado','Tecnólogo','Técnico','Licenciatura') NOT NULL COMMENT 'Tipo do curso',
+  `carga` int NOT NULL COMMENT 'Carga horária em horas',
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idcurso`),
+  CONSTRAINT `cursos_chk_1` CHECK ((`preco` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
