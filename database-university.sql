66ac8ea208ee1dc118c5b69eb66b56989f7c23f1
João Mateus <joaomateuscvieira@gmail.com>
2024-12-09 12:36:34 -0300

    Tabela alunos
    Ajuste na tabela alunos

--- database-university.sql
+++ database-university.sql
@@ -9,8 +9,8 @@ nascimento DATE NOT NULL,
 sexo ENUM ('M','F'),
 nacionalidade VARCHAR(20) DEFAULT 'Brasil',
 endereco varchar(255) NOT NULL,
-matricula TINYINT(1) NOT NULL COMMENT '1 = Ativo, 0 = Inativo',
-email VARCHAR(50) NOT NULL,
-numero VARCHAR(18) NOT NULL,
+matricula BOOLEAN NOT NULL COMMENT '1 = Ativo, 0 = Inativo',
+email VARCHAR(50) NOT NULL UNIQUE,
+numero VARCHAR(20) NOT NULL,
 PRIMARY KEY (cadastro)
 )
